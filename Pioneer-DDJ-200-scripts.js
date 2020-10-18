f1ed767866f47868d43c58a921bd2191420a6f21
Frank Breitling <frank.breitling@gmx.de>
2020-10-18 14:49:41 +0200

    Fixed errors from Code Factor

--- Pioneer-DDJ-200-scripts.js
+++ Pioneer-DDJ-200-scripts.js
@@ -1,6 +1,6 @@
 var DDJ200 = {
-    headMix_switch: 0,
-    jog_disabled: new Array(false, false, false, false, false)
+    headMixSwitch: 0,
+    jogDisabled: new Array(false, false, false, false, false)
 };
 
 DDJ200.init = function() {};
@@ -18,7 +18,8 @@ DDJ200.jog = function(channel, control, value, status, group) {
     // For a control that centers on 0x40 (64):
     // Convert value down to +1/-1
     // Register the movement
-    if (DDJ200.jog_disabled[deckNumber]) { return; }
+    var deckNumber = script.deckFromGroup(group);
+    if (DDJ200.jogDisabled[deckNumber]) { return; }
     engine.setValue(group, "jog", value - 64);
 };
 
@@ -26,8 +27,8 @@ DDJ200.touch = function(channel, control, value, status, group) {
     var deckNumber = script.deckFromGroup(group);
     if (value === 0) {
         // disable jog to not prevent alignment and enable it after 900 ms
-        DDJ200.jog_disabled[deckNumber] = true;
-        engine.beginTimer(900, "DDJ200.jog_disabled["+deckNumber+"] = false;",
+        DDJ200.jogDisabled[deckNumber] = true;
+        engine.beginTimer(900, "DDJ200.jogDisabled["+deckNumber+"] = false;",
             true);
         // disable scratch
         engine.scratchDisable(deckNumber);
@@ -50,10 +51,10 @@ DDJ200.seek = function(channel, control, value, status, group) {
 
 DDJ200.headmix = function(channel, control, value) {
     if (value === 0) { return; }
-    DDJ200.headMix_switch = 1 - DDJ200.headMix_switch;
+    DDJ200.headMixSwitch = 1 - DDJ200.headMixSwitch;
     // toggle headMix knob to values of -1 and 1
-    engine.setValue("[Master]", "headMix", 2 * DDJ200.headMix_switch - 1);
+    engine.setValue("[Master]", "headMix", 2 * DDJ200.headMixSwitch - 1);
 
     // now switch headMix LED with midi.sendShortMsg(0x96, 0x63, 0x7F or 0);
-    midi.sendShortMsg(0x90+channel, control, 0x7F * DDJ200.headMix_switch);
+    midi.sendShortMsg(0x90+channel, control, 0x7F * DDJ200.headMixSwitch);
 };
